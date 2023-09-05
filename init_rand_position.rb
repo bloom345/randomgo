dd8b9b55095a86fa0120475fc9dd5051398cd63f
odawara <odawara@hyakuren.org>
2023-09-05 12:20:05 +0900

    例外クラス修正

--- init_rand_position.rb
+++ init_rand_position.rb
@@ -1,7 +1,20 @@
 require 'date'
 # 例外クラス定義
-class BoardFullException < RuntimeError; end
-class DuplicateException < RuntimeError; end
+class BoardFullException < Exception
+  def initialize(message="cannot play any more moves!")
+    super(message)
+  end
+end
+class DuplicateException < Exception
+  def initialize(message="new move is duplicated and won't be placed on the board.")
+    super(message)
+  end
+end
+class OutOfBoardException < RuntimeError
+  def initialize(message="coordinate of the move is out of board!")
+    super(message)
+  end
+end
 
 # 一手(x, y)に打つ
 # COORDS指定時とそれ以外の共通プロセス
@@ -12,13 +25,10 @@ class DuplicateException < RuntimeError; end
 # @raise [BoardFullException] 打つ場所がないときに投げられる例外
 # @raise [DuplicateException] 既に石が置かれているところに打とうとしたときに投げられる例外
 def play(i, current_stone, x_coord, y_coord)
-  if @amount_of_stones >= BOARD_SIZE ** 2
-    raise BoardFullException.new("cannot put any more stones on the board!")
-  end
+  raise OutOfBoardException if x_coord > BOARD_SIZE || y_coord > BOARD_SIZE
+  raise BoardFullException if @amount_of_stones >= BOARD_SIZE ** 2
   new_pos = [x_coord, y_coord]
-  if @positions.include?(new_pos)
-    raise DuplicateException.new("new move is duplicated and won't be placed on the board.")
-  end
+  raise DuplicateException if @positions.include?(new_pos)
   @amount_of_stones += 1
   @positions << new_pos
   print "#{i+1}:#{current_stone}#{new_pos}, "
@@ -26,6 +36,8 @@ def play(i, current_stone, x_coord, y_coord)
   @sgf_string += "#{current_stone}[#{@num_to_alphabet[x_coord-1]}#{@num_to_alphabet[y_coord-1]}];"
 end
 
+#-----------------------------------------------------------------------------------
+# 環境変数取得
 FUSEKI_STONES_AMOUNT = (ENV["STONES"] || 10).to_i
 BOARD_SIZE = (ENV["SZ"] || 13).to_i # ボードサイズ 最大19
 MIN = (ENV["MIN"] || 2).to_i # MIN線以上に配置 MIN=3なら3線より上にしか配置しない
@@ -34,14 +46,18 @@ KOMI = (ENV["KM"] || 6.5).to_f
 RULE = ENV["RU"] || "Japanese" # Japanese or Chinese
 PB = ENV["PB"] || "black" # 黒番プレイヤー名
 PW = ENV["PW"] || "white" # 白番プレイヤー名
+
+# バリデーション
 raise "Board size should be equal to or less than 19!" if BOARD_SIZE > 19
 raise "Environment variable MIN should be less than half of board size!" if MIN > (BOARD_SIZE+1) / 2.0
+
 max_coord = BOARD_SIZE - (MIN - 1)
 @board = Array.new(BOARD_SIZE) { Array.new(BOARD_SIZE, '.')} 
 @num_to_alphabet = (:a..:s).to_a # 座標→アルファベットの変換用配列
 @positions = [] # 重複判定用
 @sgf_string = "(;GM[1]SZ[#{BOARD_SIZE}]PB[#{PB}]PW[#{PW}]KM[#{KOMI}]RU[#{RULE}];"
 @amount_of_stones = 0
+
 if COORDS
   COORDS.split(";").each_with_index do |pos_str, i|
     begin 
@@ -91,7 +107,7 @@ begin
   File.open(filename, "w") do |f|
     f.write(@sgf_string)
   end
-  puts "Succeeded to output #{filename}"
+  puts "Successfully output #{filename}"
 rescue => e
   puts "Failed to output #{filename}"
   puts "ERROR: #{e.message}"
