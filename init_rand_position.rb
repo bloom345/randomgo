1d284c67ecef03efcaf95f9e20129e8d6e235ffa
odawara <odawara@hyakuren.org>
2023-09-05 14:29:04 +0900

    リファクタ

--- init_rand_position.rb
+++ init_rand_position.rb
@@ -16,17 +16,17 @@ class OutOfBoardException < RuntimeError
   end
 end
 
-# 一手(x, y)に打つ
-# COORDS指定時とそれ以外の共通プロセス
+# (x, y)に指定の石を打つ
 # @param [Fixnum] i; 手数（実際の手数-1）
 # @param [String] current_stone; 黒か白か。"B"→黒、"W"→白
 # @param [Fixnum] x_coord; x座標。左上始点の列座標 1始まり。
 # @param [Fixnum] y_coord; y座標。左上始点の行座標 1始まり。
+# @raise [OutOfBoardException] 座標でボードの外側を指定したときに投げられる例外
 # @raise [BoardFullException] 打つ場所がないときに投げられる例外
 # @raise [DuplicateException] 既に石が置かれているところに打とうとしたときに投げられる例外
 def play(i, current_stone, x_coord, y_coord)
   raise OutOfBoardException if x_coord > BOARD_SIZE || y_coord > BOARD_SIZE
-  raise BoardFullException if @amount_of_stones >= (BOARD_SIZE-2*(MIN-1)) ** 2
+  raise BoardFullException if @amount_of_stones >= (MAX-MIN+1) ** 2
   new_pos = [x_coord, y_coord]
   raise DuplicateException if @positions.include?(new_pos)
   @amount_of_stones += 1
@@ -41,6 +41,7 @@ end
 FUSEKI_STONES_AMOUNT = (ENV["STONES"] || 10).to_i
 BOARD_SIZE = (ENV["SZ"] || 13).to_i # ボードサイズ 最大19
 MIN = (ENV["MIN"] || 2).to_i # MIN線以上に配置 MIN=3なら3線より上にしか配置しない
+MAX = BOARD_SIZE-MIN+1 
 COORDS = ENV["COORDS"] # "B[aa];W[ab]; ..." という文字列
 KOMI = (ENV["KM"] || 6.5).to_f
 RULE = ENV["RU"] || "Japanese" # Japanese or Chinese
@@ -49,11 +50,11 @@ PW = ENV["PW"] || "white" # 白番プレイヤー名
 
 # バリデーション
 raise "Board size should be equal to or less than 19!" if BOARD_SIZE > 19
-raise "Environment variable MIN should be less than half of board size!" if MIN > (BOARD_SIZE+1) / 2.0
+raise "Environment variable MIN should be less than half of board size!" if MIN > MAX
 
-max_coord = BOARD_SIZE - (MIN - 1)
+# インスタンス変数初期化
 @board = Array.new(BOARD_SIZE) { Array.new(BOARD_SIZE, '.')} 
-@num_to_alphabet = (:a..:s).to_a # 座標→アルファベットの変換用配列
+@num_to_alphabet = (:a..:z).to_a # 座標→アルファベットの変換用配列
 @positions = [] # 重複判定用
 @sgf_string = "(;GM[1]SZ[#{BOARD_SIZE}]PB[#{PB}]PW[#{PW}]KM[#{KOMI}]RU[#{RULE}];"
 @amount_of_stones = 0
@@ -76,8 +77,8 @@ else
   (FUSEKI_STONES_AMOUNT*2).times do |i|
     begin
      current_stone = (i % 2 == 0) ? "B" : "W"
-      x_coord = rand(MIN..max_coord)
-      y_coord = rand(MIN..max_coord)
+      x_coord = rand(MIN..MAX)
+      y_coord = rand(MIN..MAX)
       play(i, current_stone, x_coord, y_coord)
     rescue BoardFullException => e
       puts e.message
@@ -96,7 +97,7 @@ end
 
 @sgf_string += ")"
 puts @sgf_string
-filename ="random_SZ#{BOARD_SIZE}_NOS#{FUSEKI_STONES_AMOUNT}_#{DateTime.now.strftime('%Y%m%d%H%M%S')}.sgf" 
+filename ="sgf/random_SZ#{BOARD_SIZE}_NOS#{FUSEKI_STONES_AMOUNT}_#{DateTime.now.strftime('%Y%m%d%H%M%S')}.sgf" 
 begin
   File.open(filename, "w") do |f|
     f.write(@sgf_string)
